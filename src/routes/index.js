b77def1b2e319e01e718d73be707a706979c5df6
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-15 14:59:04 -0300

    toastify

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch } from 'react-router-dom'
+import { toast } from "react-toastify";
 import MyRoute from "./Myroute";
 
 import Login from '../pages/Login'
@@ -7,10 +8,12 @@ import Page404 from '../pages/Page404'
 
 
 export default function Routes() {
+    toast.success('oie')
+    toast.error('oie')
     return (
         
             <Switch>
-                <MyRoute exact path="/" component={Login} isClosed />
+                <MyRoute exact path="/" component={Login} />
                 <MyRoute exact path="*" component={Page404} />
 
             </Switch>

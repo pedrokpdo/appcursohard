8bc98322c2da9ca91c09de11586890ddce53ff1e
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-15 14:59:36 -0300

    toastify add

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch } from 'react-router-dom'
-import { toast } from "react-toastify";
 import MyRoute from "./Myroute";
 
 import Login from '../pages/Login'
@@ -8,8 +7,6 @@ import Page404 from '../pages/Page404'
 
 
 export default function Routes() {
-    toast.success('oie')
-    toast.error('oie')
     return (
         
             <Switch>
